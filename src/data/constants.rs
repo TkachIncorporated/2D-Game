3ef300daddfbab3d4f40d7dd882abd2cc0eb1b58
Max <doter098767890@gmail.com>
2022-04-25 04:06:09 +0300

    Test

--- src/data/constants.rs
+++
@@ -1,8 +0,0 @@
-//Camera Speed
-pub const CAMERA_SPEED_PER_SEC: f32 = 5.;
-//Player Speed
-pub const SPEED: f32 = 200.;
-//Bullet Speed
-pub const BULLET_SPEED: f32 = 600.;
-//Jump Force
-pub const JUMP_FORCE: f32 = 200.;
--- src/data/events.rs
+++
@@ -1,9 +0,0 @@
-use bevy::math::Vec2;
-
-use super::components;
-
-#[cfg_attr(feature = "debug", derive(bevy_inspector_egui::Inspectable))]
-pub struct BulletFiredEvent {
-    pub position: Vec2,
-    pub direction: components::GameDirection,
-}
