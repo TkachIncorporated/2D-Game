4ae4061e854056ad241ada636d4e16419ee95b48
Max <doter098767890@gmail.com>
2022-04-26 13:38:21 +0300

    Add parallax background, add camera follow, add ambient music, fix movement sprite bug, change project structure, probably first playable version

---
+++ scripts/camera/lib/my_gdnative_lib.d
@@ -0,0 +1,1 @@
+G:\Work\Game\Project\scripts\rust\target\release\my_gdnative_lib.dll: G:\Work\Game\Project\scripts\rust\src\camera.rs G:\Work\Game\Project\scripts\rust\src\lib.rs
---
+++ scripts/player/lib/my_gdnative_lib.d
@@ -0,0 +1,1 @@
+G:\Work\Game\Project\scripts\rust\target\release\my_gdnative_lib.dll: G:\Work\Game\Project\scripts\rust\src\controls.rs G:\Work\Game\Project\scripts\rust\src\lib.rs G:\Work\Game\Project\scripts\rust\src\player.rs
