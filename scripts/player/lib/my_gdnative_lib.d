4f6e7714b07c1c6c0b713d293c8ce5607d02ccf1
Max <doter098767890@gmail.com>
2022-04-28 11:01:02 +0300

    fix actual

--- scripts/player/lib/my_gdnative_lib.d
+++ scripts/player/lib/my_gdnative_lib.d
@@ -1,1 +1,1 @@
-G:\Work\Game\Project\scripts\rust\target\release\my_gdnative_lib.dll: G:\Work\Game\Project\scripts\rust\src\controls.rs G:\Work\Game\Project\scripts\rust\src\lib.rs G:\Work\Game\Project\scripts\rust\src\player.rs
+G:\Work\Game\Project\scripts\rust\target\debug\my_gdnative_lib.dll: G:\Work\Game\Project\scripts\rust\src\camera.rs G:\Work\Game\Project\scripts\rust\src\controls.rs G:\Work\Game\Project\scripts\rust\src\lib.rs G:\Work\Game\Project\scripts\rust\src\player.rs
