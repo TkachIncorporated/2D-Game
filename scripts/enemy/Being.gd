44665dacaea6ed867fbadbd40a742b8eb112f0ce
Tkach-Illia <50165269+Tkach-Illia@users.noreply.github.com>
2022-04-28 15:46:35 +0300

    Add files via upload

---
+++ scripts/enemy/Being.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+
+var speed = 200
+
+onready var hp = 50
+export var max_hp = 100
+var bite_strength = 10
+
+func set_start_hp(hp, max_hp):
+	$HP_bar.value=hp
+	$HP_bar.max_value=max_hp
+	
+func update_hp():
+	$HP_bar.value=hp
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func reduce_hp(val):
+	self.hp-=val
+	print("bite")
+	update_hp()
---
+++ scripts/enemy/Enemy.gd
@@ -0,0 +1,85 @@
+extends "res://scripts/enemy/Being.gd"
+
+var stands = true
+var destination = Vector2()
+var velocity = Vector2()
+var prev_pos = Vector2()
+
+var target = null
+
+var target_intercepted = false
+var can_bite = true
+
+func bite(targ):
+	targ.reduce_hp(bite_strength)
+	can_bite = false
+	$BiteTimer.start(1)
+	
+func _ready() -> void:
+	self.hp = 90
+	set_start_hp(self.hp,self.max_hp)
+	
+func _process(delta):
+	if velocity:
+		prev_pos = position
+		move_and_slide(velocity)
+	wander()
+	search_for_target()
+	
+	if target_intercepted and can_bite:
+		bite(target)
+
+func set_destination(dest):
+	destination=dest
+	velocity = (destination - position).normalized()*speed
+	stands = false
+	
+func search_for_target():
+	var pl = get_parent().get_parent().get_child(0)
+	if position.distance_to(pl.position) < 200:
+		cancel_move()
+		target = pl
+	else:
+		if target:
+			cancel_move()
+		target = null
+	if target:
+		set_destination(target.position)
+
+func cancel_move():
+	velocity = Vector2()
+	destination = Vector2()
+	$StandTimer.start(2)
+	
+func wander():
+	var pos = position
+	if stands:
+		randomize()
+		var x = int(rand_range(pos.x - 1500,pos.x+1500))
+		var y = int(rand_range(pos.y - 150, pos.y+150))
+		set_destination(Vector2(x,y))
+	elif velocity != Vector2():
+		if pos.distance_to(destination) <=speed:
+			cancel_move()
+		elif pos.distance_to(prev_pos)<=2:
+			cancel_move()
+			
+func _on_StandTimer_timeout() -> void:	
+	stands = true
+	pass
+
+func _on_BiteTimer_timeout() -> void:
+	can_bite = true
+	pass
+
+
+func _on_HitBox_area_entered(area: Area2D) -> void:
+	if area.get_parent() == target:
+		target_intercepted = true	
+	pass # Replace with function body.
+
+
+func _on_AttackArea_area_exited(area: Area2D) -> void:
+	if area.get_parent() == target:
+		target_intercepted = false	
+	pass # Replace with function body.
