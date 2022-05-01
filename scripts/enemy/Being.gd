7e56a2a9995366e439c2ee378b9a661dc2ea755d
Tkach <tkachillia01@gmail.com>
2022-05-01 20:30:22 +0300

    Небольшие изменения

--- scripts/enemy/Being.gd
+++ scripts/enemy/Being.gd
@@ -6,6 +6,11 @@ onready var hp = 50
 export var max_hp = 100
 var bite_strength = 10
 
+var target_name = "Player"
+var target = null
+
+
+
 func set_start_hp(hp, max_hp):
 	$HP_bar.value=hp
 	$HP_bar.max_value=max_hp
@@ -13,15 +18,21 @@ func set_start_hp(hp, max_hp):
 func update_hp():
 	$HP_bar.value=hp
 
-func _process(_delta):
-	if self.hp == 0:
+func _process(delta):
+	death_check()
+
+func death_check():
+	if self.hp <= 0:
 		self.queue_free()
 
 func _ready() -> void:
+	target = get_parent().get_child(0)
+	print(target)
 	pass # Replace with function body.
 
 func reduce_hp(val):
 	self.hp-=val
-	print("bite")
 	update_hp()
 	print(self.hp)
+
+
--- scripts/enemy/Enemy.gd
+++ scripts/enemy/Enemy.gd
@@ -5,86 +5,89 @@ var destination = Vector2()
 var velocity = Vector2()
 var prev_pos = Vector2()
 
-var target = null
-
-
 var target_intercepted = false
 var can_bite = true
+var target_obviously = false
+var can_attack = true
 
-func bite(targ):
-	targ.reduce_hp(bite_strength)
-	can_bite = false
-	$BiteTimer.start(1)
-	
 func _ready() -> void:
+	self.speed = 700
 	self.hp = 90
 	set_start_hp(self.hp,self.max_hp)
 	
 func _process(delta):
+	base_attack()
+	death_check()
+	
+func base_attack():
 	if velocity:
 		prev_pos = position
 		move_and_slide(velocity)
-	wander()
-	search_for_target()
 	
 	if target_intercepted and can_bite:
 		bite(target)
+		
+	if target_obviously and can_attack:
+		can_attack = false
+		$AttackTimer.start(5)
+		set_destination(target.position)
+	
+func bite(targ):
+	targ.reduce_hp(bite_strength)
+	can_bite = false
+	$BiteTimer.start(2)
 
+func cancel_move():
+	velocity = Vector2()
+	destination = Vector2()
+	
 func set_destination(dest):
 	destination=dest
 	velocity = (destination - position).normalized()*speed
 	stands = false
 	
-func search_for_target():
-	var pl = get_parent().get_child(0)
-	if position.distance_to(pl.position) < 200:
-		cancel_move()
-		target = pl
-	else:
-		if target:
-			cancel_move()
-		target = null
-	if target:
-		set_destination(target.position)
-
-func cancel_move():
-	velocity = Vector2()
-	destination = Vector2()
-	$StandTimer.start(2)
-	
-func wander():
-	var pos = position
-	if stands:
-		randomize()
-		var x = int(rand_range(pos.x - 1500,pos.x+1500))
-		var y = int(rand_range(pos.y - 150, pos.y+150))
-		set_destination(Vector2(x,y))
-	elif velocity != Vector2():
-		if pos.distance_to(destination) <=speed:
-			cancel_move()
-		elif pos.distance_to(prev_pos)<=2:
-			cancel_move()
-			
-func _on_StandTimer_timeout() -> void:	
-	stands = true
-	pass
 
 func _on_BiteTimer_timeout() -> void:
 	can_bite = true
 	pass
 
+func _on_DetectionArea_area_entered(area: Area2D) -> void:
+	if area.get_parent().get_parent().name == target_name:
+		target = area.get_parent().get_parent()
+		target_obviously = true
+		set_destination(target.position)
+	pass # Replace with function body.
 
-func _on_HitBox_area_entered(area: Area2D) -> void:
-	if area.get_parent() == target:
+func _on_DetectionArea_area_exited(area: Area2D) -> void:
+	if area.get_parent().get_parent() == target:
+		target_obviously = false
+		$TimerSearch.start(4)
+		velocity=-velocity
+	pass # Replace with function body.
+
+
+func _on_AttackArea_area_entered(area: Area2D) -> void:
+	if area.get_parent().get_parent() == target:
 		target_intercepted = true	
 	pass # Replace with function body.
 
 
 func _on_AttackArea_area_exited(area: Area2D) -> void:
-	if area.get_parent() == target:
+	if area.get_parent().get_parent() == target:
 		target_intercepted = false	
 	pass # Replace with function body.
 
+func _on_HitBox_area_entered(area: Area2D) -> void:
+	print("2")
+	if area.is_in_group("Weapon"):
+		print("wdqd")
+		reduce_hp(area.get_parent().get_parent().DAMAGE)
+		
+func _on_AttackTimer_timeout() -> void:
+	can_attack = true
+	pass # Replace with function body.
 
-func _on_Timer_timeout():
+
+func _on_TimerSearch_timeout() -> void:
+	cancel_move()
 	pass # Replace with function body.
