c1fd9eb80e9946229d2eb83c30dd2f9b61471947
Petsolyak Maxim <80517435+TkachIncorporated@users.noreply.github.com>
2022-05-02 15:11:04 +0300

    add main menu

---
+++ scripts/mainmenu/MainMenu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+	$VBoxContainer/Start.grab_focus()
+
+func _on_Start_pressed():
+	var _scene = get_tree().change_scene("res://scenes/Main.tscn")
+
+func _on_Exit_pressed():
+	get_tree().quit()
