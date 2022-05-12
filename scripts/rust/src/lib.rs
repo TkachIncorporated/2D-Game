7ec63eb26063751db4029c3eb95e8c0b004a461b
Antonov Volodymyr <azazaka2002@gmail.com>
2022-05-12 23:14:00 +0300

    inventory first impl

--- scripts/rust/src/lib.rs
+++ scripts/rust/src/lib.rs
@@ -1,12 +1,15 @@
 use gdnative::init::{godot_gdnative_init, godot_gdnative_terminate, godot_nativescript_init};
+use inventory::Inventory;
 use player::Player;
 
 mod camera;
 mod controls;
+mod inventory;
 mod player;
 
 fn init(handle: gdnative::init::InitHandle) {
     handle.add_class::<Player>();
+    handle.add_class::<Inventory>();
 }
 
 godot_gdnative_init!();
