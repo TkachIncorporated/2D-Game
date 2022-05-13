e9099e8a07b5c613f3a8600395acd1107abe1499
Antonov Volodymyr <azazaka2002@gmail.com>
2022-05-13 16:21:43 +0300

    align fix

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -7,8 +7,8 @@ const JUMP_SPEED = 400
 const GRAVITY = 1000
 
 var velocity = Vector2.ZERO
-var is_attacking = false;
-var inventory =false ;
+var is_attacking = false
+var inventory = false
 
 func _ready():
 	pass 
@@ -29,27 +29,26 @@ func _process(delta):
 		if !is_attacking:
 			sprite.play("Idle")
 
-		var is_jumping = Input.is_action_just_pressed("jump") and is_on_floor()
-		
-		if Input.is_action_just_pressed("jump") && is_jumping:
-			velocity.y = -JUMP_SPEED
-		
-		velocity.y += GRAVITY * delta;	
-		velocity = move_and_slide(velocity, Vector2.UP)
-		
-		if Input.is_action_pressed("attack"):
-			sprite.play("Attack")
-			$Scaling/AttackArea/AttackCollider.disabled = false
-			is_attacking = true
-			$Inventory.add_item("Slime", 1)
-
-		if Input.is_action_just_pressed("ui_home") and not inventory:
+	var is_jumping = Input.is_action_just_pressed("jump") and is_on_floor()
+	
+	if Input.is_action_just_pressed("jump") && is_jumping:
+		velocity.y = -JUMP_SPEED
+	
+	velocity.y += GRAVITY * delta;	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	if Input.is_action_pressed("attack"):
+		sprite.play("Attack")
+		$Scaling/AttackArea/AttackCollider.disabled = false
+		is_attacking = true
+		$Inventory.add_item("Slime", 1)
+	
+	if Input.is_action_just_pressed("ui_home") and not inventory:
 			$Inventory.show()
 			inventory=true
-		elif Input.is_action_just_pressed("ui_home"):
+	elif Input.is_action_just_pressed("ui_home"):
 			$Inventory.hide()
 			inventory=false
-
 	
 
 func _on_AnimatedSprite_animation_finished():
